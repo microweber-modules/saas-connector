648de651f28e05d0173d9b304c625296c6324ffd
Bozhidar Slaveykov <bobi@microweber.com>
2023-02-01 13:05:58 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+$config = array();
+$config['name'] = "Saas Connector";
+$config['author'] = "Microweber";
+
+$config['categories'] = "admin";
+$config['version'] = 0.1;
+$config['ui_admin'] = true;
+$config['ui'] = false;
+$config['position'] = 99;
+
+$config['settings'] = [];
+$config['settings']['autoload_namespace'] = [
+    [
+        'path' => __DIR__ . '/src/',
+        'namespace' => 'MicroweberPackages\\Modules\\SaasConnector\\'
+    ],
+];
+$config['settings']['service_provider'] = [
+    \MicroweberPackages\Modules\SaasConnector\SaasConnectorServiceProvider::class
+];
