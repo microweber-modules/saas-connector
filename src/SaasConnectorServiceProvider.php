55653829d73a5083ea14b6c19f9317fcd606ba0e
Bozhidar Slaveykov <bobi@microweber.com>
2023-02-01 13:08:10 +0200

    Create SaasConnectorServiceProvider.php

---
+++ src/SaasConnectorServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MicroweberPackages\Modules\SaasConnector;
+
+use Illuminate\Support\ServiceProvider;
+
+class SaasConnectorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      
+    }
+
+    public function register()
+    {
+      
+
+    }
+}
