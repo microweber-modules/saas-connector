a67e8cddd19baf89c64d653c89de5c9cc086428c
Nikolay <rachkuw@gmail.com>
2023-02-08 15:30:12 +0200

    update

--- src/resources/views/components/setup-wizard.blade.php
+++ src/resources/views/components/setup-wizard.blade.php
@@ -37,53 +37,60 @@
 
         <div class="row mt-4">
             <div class="col-md-2">
-                <b>Categories</b>
-                <br />
-                <a href="<?php echo site_url('setup-wizard'); ?>">All</a>
-                <br />
-            @foreach($siteTemplateCategories as $category)
-                    <a href="?category={{$category['slug']}}">{{$category['name']}}</a> <br />
-                @endforeach
-            </div>
-            <div class="col-md-10">
-            <div class="row">
-            @foreach($siteTemplates as $template)
 
-                <div class="col-xl-4 col-md-6 col-12">
+                <div class="mw-process-categories-wrapper">
+                    <span id="category-marker"></span>
+                    
+                    <a href="<?php echo site_url('setup-wizard'); ?>">
 
-                    <h4>{{$template['name']}}</h4>
+                        All
+                    </a>
 
-                    @if(isset($template['screenshot']))
-
-                         <img src="{{$template['screenshot']}}" class="img-responsive" />
-
-                    @endif
-
-                    <div>
-
-                        @if (!empty($template['categories']))
-                            @foreach($template['categories'] as $category)
-                                <a href="?category={{$category['slug']}}">{{$category['name']}}</a>
-                            @endforeach
-                            @php
-                                $categoriesPlain = array_column($template['categories'], 'name');
-                                $categoriesPlain = implode(', ', $categoriesPlain);
-                            @endphp
-                           <div>
-                               <b>
-                                   {{$categoriesPlain}}
-                               </b>
-                           </div>
-                        @endif
-                    </div>
-
-                    <button type="button" data-template="{{$template['dir_name']}}" class="btn mt-3 btn-primary js-start-with-this-template">
-                        Start With This Template
-                    </button>
+                    @foreach($siteTemplateCategories as $category)
+                        <a href="?category={{$category['slug']}}">{{$category['name']}}</a>
+                    @endforeach
                 </div>
 
-            @endforeach
             </div>
+            <div class="col-md-10">
+                <div class="row">
+                    @foreach($siteTemplates as $template)
+
+                        <div class="col-xl-4 col-md-6 col-12">
+
+                            <h4>{{$template['name']}}</h4>
+
+                            @if(isset($template['screenshot']))
+
+                                 <img src="{{$template['screenshot']}}" class="img-responsive" />
+
+                            @endif
+
+                            <div>
+
+                                @if (!empty($template['categories']))
+                                    @foreach($template['categories'] as $category)
+                                        <a href="?category={{$category['slug']}}">{{$category['name']}}</a>
+                                    @endforeach
+                                    @php
+                                        $categoriesPlain = array_column($template['categories'], 'name');
+                                        $categoriesPlain = implode(', ', $categoriesPlain);
+                                    @endphp
+                                   <div>
+                                       <b>
+                                           {{$categoriesPlain}}
+                                       </b>
+                                   </div>
+                                @endif
+                            </div>
+
+                            <button type="button" data-template="{{$template['dir_name']}}" class="btn mt-3 btn-primary js-start-with-this-template">
+                                Start With This Template
+                            </button>
+                        </div>
+
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
