51ba1bbec618cd31462d71fa0b3a0cbb9689c379
Nikolay <rachkuw@gmail.com>
2023-02-13 10:36:54 +0200

    update

--- src/resources/views/components/setup-wizard.blade.php
+++ src/resources/views/components/setup-wizard.blade.php
@@ -39,14 +39,13 @@
                 <h6 class="mw-templates-header text-center">Create New Site</h6>
                 <p class="mw-templates-text text-center">Choose a sutable template and customize it to fit your <br> style and ideas</p>
 
-                <div style="max-height: 120px;overflow: hidden;" class="mw-process-categories-wrapper d-flex flex-wrap justify-content-center align-items-center">
-                    <a <?php if (!isset($_GET['category'])): ?> class="active" <?php endif; ?> href="<?php echo site_url('setup-wizard'); ?>">
+                <div class="mw-process-categories-wrapper d-flex flex-wrap justify-content-center align-items-center">
+                    <a <?php if (!isset($_GET['category'])): ?> class="active m-lg-0 m-2" <?php endif; ?> href="<?php echo site_url('setup-wizard'); ?>">
                         All
                     </a>
 
-
                     @foreach($siteTemplateCategories as $category)
-                        <a <?php if (isset($_GET['category']) && $_GET['category'] == $category['slug']): ?> class="active" <?php endif; ?> href="?category={{$category['slug']}}">
+                        <a <?php if (isset($_GET['category']) && $_GET['category'] == $category['slug']): ?> class="active m-lg-0 m-2" <?php endif; ?> href="?category={{$category['slug']}}">
                             {{$category['name']}}
                         </a>
                     @endforeach
