222aa390d420fcadc7ceee24d15857c6466c9a11
Bozhidar Slaveykov <bobi@microweber.com>
2023-10-27 13:02:03 +0300

    update

--- functions.php
+++ functions.php
@@ -116,6 +116,22 @@ if (isset($checkWebsite['appendScriptsAdminPanel']) && !empty($checkWebsite['app
     });
 }
 
+$hasActiveSubscription = false;
+if (isset($checkWebsite['activeSubscription']) && !empty($checkWebsite['activeSubscription'])) {
+    $hasActiveSubscription = true;
+}
+
+if (!$hasActiveSubscription) {
+
+    event_bind('mw.controller.index', function () use($checkWebsite) {
+
+        echo view('saas_connector::upgrade-plan');
+        exit;
+
+    });
+
+}
+
 event_bind('mw.front', function () use($checkWebsite) {
 
     if (!in_live_edit()) {
@@ -137,52 +153,53 @@ event_bind('mw.front', function () use($checkWebsite) {
     }
 
 });
-if ($canIShowExternalAds and !in_live_edit()) {
 
-    if (isset($checkWebsite['appendScriptsExternalAdsFrontend']) && !empty($checkWebsite['appendScriptsExternalAdsFrontend'])) {
-        event_bind('mw.front', function () use($checkWebsite) {
-            mw()->template->foot($checkWebsite['appendScriptsExternalAdsFrontend']);
-        });
-    }
-}
-
-if ($canIShowAdsBar and !in_live_edit()) {
-
-    event_bind('mw.front', function () {
-
-        $css = '
-        <style>
-        .js-microweber-add-iframe-wrapper {
-            height: 40px;
-            width: 100%;
-            min-height: 40px !important;
-            max-height: 40px !important;
-            position:relative;
-        }
-        .js-microweber-add-iframe {
-            z-index: 99999;
-            position: fixed;
-            min-height: 0;
-            height: 40px !important;
-            border: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            width: 100%;
-            overflow: hidden;
-        }
-        </style>
-    ';
-
-        if (is_live_edit()) {
-            $url = '/ads-bar?live_edit=1';
-        } else {
-            $url = '/ads-bar';
-        }
-
-        mw()->template->foot($css . '<div class="js-microweber-add-iframe-wrapper">
-                 <iframe class="js-microweber-add-iframe" scrolling="no" frameborder="0" src="' . $url . '"></iframe>
-            </div>');
-
-    });
-}
+//if ($canIShowExternalAds and !in_live_edit()) {
+//
+//    if (isset($checkWebsite['appendScriptsExternalAdsFrontend']) && !empty($checkWebsite['appendScriptsExternalAdsFrontend'])) {
+//        event_bind('mw.front', function () use($checkWebsite) {
+//            mw()->template->foot($checkWebsite['appendScriptsExternalAdsFrontend']);
+//        });
+//    }
+//}
+//
+//if ($canIShowAdsBar and !in_live_edit()) {
+//
+//    event_bind('mw.front', function () {
+//
+//        $css = '
+//        <style>
+//        .js-microweber-add-iframe-wrapper {
+//            height: 40px;
+//            width: 100%;
+//            min-height: 40px !important;
+//            max-height: 40px !important;
+//            position:relative;
+//        }
+//        .js-microweber-add-iframe {
+//            z-index: 99999;
+//            position: fixed;
+//            min-height: 0;
+//            height: 40px !important;
+//            border: 0;
+//            left: 0;
+//            right: 0;
+//            bottom: 0;
+//            width: 100%;
+//            overflow: hidden;
+//        }
+//        </style>
+//    ';
+//
+//        if (is_live_edit()) {
+//            $url = '/ads-bar?live_edit=1';
+//        } else {
+//            $url = '/ads-bar';
+//        }
+//
+//        mw()->template->foot($css . '<div class="js-microweber-add-iframe-wrapper">
+//                 <iframe class="js-microweber-add-iframe" scrolling="no" frameborder="0" src="' . $url . '"></iframe>
+//            </div>');
+//
+//    });
+//}
---
+++ src/resources/views/upgrade-plan.blade.php
@@ -0,0 +1,41 @@
+<div>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Raleway', sans-serif;
+            color: #2b2b2b;
+            background: #efefef;
+        }
+        a {
+            color: #1f1f1f;
+        }
+    </style>
+
+    <div style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100vh;
+">
+
+        <div>
+            <h1>
+                Site not published yet
+            </h1>
+        </div>
+
+        <div>
+            <p>
+                This site is currently not published yet. If you’re the owner or contributor, log in to
+                <a href="https://microweber.com/projects">Microweber</a>.
+            </p>
+        </div>
+
+    </div>
+
+</div>
