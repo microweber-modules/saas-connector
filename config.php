97d5ac2dceb726488f30bef924383e1c5a049d80
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-20 12:53:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $config['name'] = "Saas Connector";
 $config['author'] = "Microweber";
 
 $config['categories'] = "admin";
-$config['version'] = "1.4";
+$config['version'] = "1.5";
 $config['ui_admin'] = true;
 $config['ui'] = false;
 $config['position'] = 99;
