5d9a696302e644a2233dd4d2078188ab25d584e3
Peter Ivanov <peter@microweber.com>
2024-03-15 12:45:31 +0200

    update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $config['name'] = "Saas Connector";
 $config['author'] = "Microweber";
 
 $config['categories'] = "admin";
-$config['version'] = "1.9";
+$config['version'] = "2.0";
 $config['ui_admin'] = true;
 $config['ui'] = false;
 $config['position'] = 99;
